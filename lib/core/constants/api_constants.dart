73811509ba36be0895ec0e82139a7564629f23f6
mohamed ibrahem <mohamed.data13@gmail.com>
2024-10-16 13:06:02 +0300

    api key comment

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,6 +1,7 @@
 class ApiConstants {
   static const String baseUrl = 'https://api.openweathermap.org/data/2.5/';
 
+  /// this can be hidden but i let it be for now to test from your side(MD-Labs)
   static const String apiKey = 'a40daafc40b0c710dcf2a8e94e0fb059';
   static const String weather = 'weather';
 }
